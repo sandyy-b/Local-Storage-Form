af24cde7a2cad999d0518ab149ec86856990d729
sandy <sandy.b@appwrk.com>
2022-05-03 17:58:55 +0530

    Final Modifications Done

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let id = "";
 // localStorage.clear();
-
 window.addEventListener('load', function () {
     let formInput = document.querySelectorAll('.form-control');
     for (var i = 0; i < formInput.length; i++) {
@@ -18,24 +17,42 @@ window.addEventListener('load', function () {
 
 
 function validation() {
+    let validate = 0;
+
     let fname = document.getElementById("inputFirstName").value;
     let lname = document.getElementById("inputLastName").value;
     let email = document.getElementById("inputEmail").value;
+    let emailRegex = /^[A-Za-z._]{3,}@[A-Za-z._]{3,}[.]{1}[A-Za-z]{2,6}$/;
     let password = document.getElementById("inputPassword").value;
     let confirmPassword = document.getElementById("inputConfirmPassword").value;
-    let phoneNumber = document.getElementById("inputPhoneNumber").value;
-    let inputGender = document.querySelector('#messageGender:checked');
+    let phoneNumber = document.getElementById("inputPhoneNumber").value;    
+    let phoneNumberRegex = "^[0-9]{10}$";
+    let inputGender = document.querySelector('input[name="gender"]:checked');
 
     if (fname == '') {
-        document.getElementById("messageFirstName").innerHTML = "Please Enter your First Name";
+        document.getElementById("messageFirstName").innerHTML =
+            "Please Enter your First Name";
+    }
+    else if (!isNaN(fname.match(/^[A-Za-z]+$/))) {
+        document.getElementById("messageFirstName").innerHTML =
+            "Please Enter a Valid First Name";
     }
     if (lname == '') {
         document.getElementById("messageLastName").innerHTML =
             "Please Enter your Last Name";
     }
+    else if (!isNaN(lname.match(/^[A-Za-z]+$/))) {
+        document.getElementById("messageLastName").innerHTML =
+            "Please Enter a Valid Last Name";
+    }
     if (email == '') {
         document.getElementById("messageEmail").innerHTML =
             "Please Enter your Email Id";
+    }
+    else if (!email.match(emailRegex)) {
+        document.getElementById("messageEmail").innerHTML =
+            "Please Enter a Valid Email Id";
+            validate = 1;
     }
     if (password == '') {
         document.getElementById("messagePassword").innerHTML =
@@ -45,20 +62,25 @@ function validation() {
         document.getElementById("messageConfirmPassword").innerHTML =
             "Please Confirm your Password";
     }
+    if (password != confirmPassword) {
+        document.getElementById("messageConfirmPassword").innerHTML = "Passwords did not match";
+    }
     if (phoneNumber == '') {
         document.getElementById("messagePhoneNumber").innerHTML =
             "Please Enter your Phone Number";
+            validate = 1;
     }
-    // if (inputGender == '') {
-    //     document.getElementById("messageGender").innerHTML =
-    //         "Please Enter your Gender";
-    // }
-    else if (password != confirmPassword) {
-        document.getElementById("messageCheckPassword").innerHTML = 
-        "<h3 class = 'text-danger'>Passwords did not match</h3>";
+    else if (!phoneNumber.match(phoneNumberRegex)) {
+        document.getElementById("messagePhoneNumber").innerHTML =
+            "Please Enter a valid Phone Number";
+            validate = 1;
     }
-    
-    else {
+    if (inputGender == null) {
+        document.getElementById("messageGender").innerHTML =
+            "Please Enter your Gender";
+    }
+
+    if(validate === 0) {
         setData();
     }
 
@@ -79,15 +101,5 @@ function setData() {
     })
     user_records = localStorage.setItem("users", JSON.stringify(user_records));
     window.location.href = 'table.html';
+    document.getElementById("reset").click();
 }
-
-function matchPassword() {
-    let inputPassword = document.getElementById("inputPassword");
-    let inputConfirmPassword = document.getElementById("inputConfirmPassword");
-    if (inputPassword != inputConfirmPassword) {
-        document.getElementById("messageCheckPassword").innerHTML = "<h3 class = 'text-danger'>Passwords did not match</h3>";
-    } 
-    else {
-        // document.getElementById("messageCheckPassword").innerHTML = "<h3 class = 'text-success'>Login Successfull</h3>";
-    }
-} 
